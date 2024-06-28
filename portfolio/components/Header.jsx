75383dffad5e3646b03e16d04e5c02a9ad75c8c8
pyjpg <titas.r@outlook.com>
2024-06-28 22:54:16 +0100

    Header

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from "next/link"
+import { Button } from "./ui/button"
+import Nav from "./Nav"
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-        <div className="container mx-auto">Header</div>
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            <Link href="/">
+            <h1 className="text-4xl font-semibold">Titas<span className="text-accent">.</span></h1>
+            </Link>
+            {/* desktop view */}
+            <div className="hidden xl:flex">
+                <Nav />
+                <Link href="/contact">
+                    <Button>Hire me</Button>
+                </Link>
+            </div>
+            
+            <div className="xl:hidden">mobile nav</div>
+        </div>
     </header>
   )
 }
