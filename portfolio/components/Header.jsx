afc3f703b52a8953bf27578d3d83cf0298718eaa
pyjpg <titas.r@outlook.com>
2024-06-28 18:55:21 +0100

    Initial Commit

---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+        <div className="container mx-auto">Header</div>
+    </header>
+  )
+}
+
+export default Header
