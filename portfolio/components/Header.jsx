4f1b7e75fb9dfe89963690993e324e0feab01a52
pyjpg <titas.r@outlook.com>
2024-06-29 12:51:59 +0100

    Finished NavBar + Header components

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,26 +1,31 @@
-import Link from "next/link"
-import { Button } from "./ui/button"
-import Nav from "./Nav"
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
-            <Link href="/">
-            <h1 className="text-4xl font-semibold">Titas<span className="text-accent">.</span></h1>
-            </Link>
-            {/* desktop view */}
-            <div className="hidden xl:flex">
-                <Nav />
-                <Link href="/contact">
-                    <Button>Hire me</Button>
-                </Link>
-            </div>
-            
-            <div className="xl:hidden">mobile nav</div>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Titas<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop view */}
+        <div className="hidden xl:flex items-center">
+          <Nav />
+          <Link href="/contact">
+            <Button className="ml-8">Hire me</Button>
+          </Link>
         </div>
-    </header>
-  )
-}
 
-export default Header
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
